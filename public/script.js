27c4eeacaf97356cf2e8440546dfabab8df441eb
dkomin415 <dkomin415.sales@gmail.com>
2022-08-22 16:45:52 -0700

    added open and close functionality to modal

--- public/script.js
+++ public/script.js
@@ -1,8 +1,17 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var submitBtn = document.querySelector("#submit");
+var modalContainer = document.querySelector("#modal_container");
+
+var generatePassword = function() {
+  var password = 'password';
+
+  return password;
+}
+
+var triggerModal = function() {
+  modalContainer.classList.add('show');
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +19,12 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+submitBtn.addEventListener("click", function(){
+  modalContainer.classList.remove('show');
+  writePassword();
+})
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", triggerModal);
