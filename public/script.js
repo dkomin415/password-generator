a5be4705891ba9e12c1bcc83fe6498543a76a551
dkomin415 <dkomin415.sales@gmail.com>
2022-08-24 22:28:38 -0700

    completed copy feature

--- public/script.js
+++ public/script.js
@@ -2,11 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 var submitBtn = document.querySelector("#submit");
 var modalContainer = document.querySelector("#modal_container");
+var passwordText = document.querySelector("#password");
 var lengthEl = document.querySelector("#length");
 var upperCase = document.querySelector("#uppercase");
 var lowerCase = document.querySelector("#lowercase");
 var numbers = document.querySelector("#numbers");
 var specChar = document.querySelector("#spec-char");
+var copyText = document.querySelector("#copy");
 
 var randomNumber = function() {
   var randomNum = Math.floor(Math.random() * 10);
@@ -50,22 +52,25 @@ var generatePassword = function() {
     arr.push(4);
   }
 
-
+  
   for (var i = 0; i < length; i++) {
     var randomNum = Math.floor(Math.random() * arr.length);
     switch (arr[randomNum]) {
       case 1:
         password += randomLowerChar();
         break;
+      
       case 2: 
         password += randomUpperChar();
         break;
+
       case 3: 
         password += randomNumber();
         break;
+        
       case 4: 
         password += randomSpecialChar();
-    }
+      }
   }
 
   console.log(password);
@@ -79,15 +84,39 @@ var triggerModal = function() {
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
 submitBtn.addEventListener("click", function(){
   modalContainer.classList.remove('show');
+  if (!lowerCase.checked && !upperCase.checked && !numbers.checked && !specChar.checked) {
+    alert("Error: Please Check One Of The Boxes");
+    triggerModal();
+  }
   writePassword();
-})
+});
+
+function displayCopy() {
+  var timeleft = 1;
+  copyText.innerHTML = "Copied";
+  var downloadTimer = setInterval(function(){
+    copyText.innerHTML = "Click the area to copy"
+    timeleft--;
+  }, 1500);
+
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+  }
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", triggerModal);
+
+
+passwordText.addEventListener("click", function() {
+  passwordText.select();
+  passwordText.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(passwordText.value);
+  displayCopy();
+});
